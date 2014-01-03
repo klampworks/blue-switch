f13c8514ab62d37eb0d7e9c672919537cad22c8d
yukipyon <wolfvbear@gmail.com>
2014-01-03 15:41:46 +0000

    Added comments to make sure I understand the code.

--- dummy/rfcomm-client.c
+++ dummy/rfcomm-client.c
@@ -10,19 +10,26 @@ int main()
 	int s, status;
 	char dest[18] = "??:??:??:??:??:??";
 
+	//ALlocate a new socket. */
 	s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
 	addr.rc_family = AF_BLUETOOTH;
+	/* Destination port 1. */
 	addr.rc_channel = (uint8_t) 1;
+	/* Convert our textual MAC address t a bdaddr_t struct. */
 	str2ba(dest, &addr.rc_bdaddr);
 
+	/* Connect to the remote endpoint. */
 	status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
 	if (status == 0)
+		/* Send data to the endpoint. */
 		status = write(s, "ahoy", 5);
+	else
+		puts("Connection failed.");
 
 	if (status < 0)
-		perror("Unspecified error.");
+		puts("Write failed.");
 
 	close(s);
 	return 0;
